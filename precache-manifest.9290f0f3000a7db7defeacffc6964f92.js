6ebda467ffd587efcc1854d2ed404b608f353ef2
Harshal Mahajan <hmahajan@calibo.com>
2021-08-11 14:14:09 +0530

    Updates

---
+++ precache-manifest.9290f0f3000a7db7defeacffc6964f92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12f130bb3db47cc639d1171757ca1708",
+    "url": "/react-skeleton-animation/index.html"
+  },
+  {
+    "revision": "d36f8450ea98591e1cca",
+    "url": "/react-skeleton-animation/static/css/main.41b4056b.chunk.css"
+  },
+  {
+    "revision": "66df38202c1ab6c82d30",
+    "url": "/react-skeleton-animation/static/js/2.eca931ff.chunk.js"
+  },
+  {
+    "revision": "ae6bb62977b25ec73f0509e3cbccba19",
+    "url": "/react-skeleton-animation/static/js/2.eca931ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d36f8450ea98591e1cca",
+    "url": "/react-skeleton-animation/static/js/main.d27fcb6d.chunk.js"
+  },
+  {
+    "revision": "0cda083c8c169faa4d02",
+    "url": "/react-skeleton-animation/static/js/runtime-main.8b88cee3.js"
+  }
+]);
